62688dc58625cf0b73a3600d6d3d3dbec46bdbac
kochan4php <aprodeosubarno@gmail.com>
2023-06-10 12:48:48 +0700

    perbaikan kecil

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import cors from "cors";
 import express from "express";
 import Route from "./routes/index.js";
 
 const app = express();
 
+app.use(cors());
 app.use("/api", Route);
 app.listen(8000, () => console.log("Server in port 8000"));
