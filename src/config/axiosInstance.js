72bd3b0a615eda8bc2d11d61dfa1bda1c15d079a
kochan4php <aprodeosubarno@gmail.com>
2023-06-10 13:49:55 +0700

    menambahkan fake user agent

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -13,7 +13,7 @@ const axiosInstance = axios.create({
         "Content-Type": "application/x-www-form-urlencoded",
         Accept: "application/json, text/javascript, */*; q=0.01",
         "X-Requested-With": "XMLHttpRequest",
-        "User-Agent": fakeUseragent(),
+        "User-Agent": "*",
         Referer: "https://kusonime.com/",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9,id;q=0.8",
